2d7dc6496a94e8f96d6e19d4df3aabe26d88af14
Mr-houu <80619275+Mr-houu@users.noreply.github.com>
2021-06-24 00:21:04 +0800

    Update App.java

--- src/main/java/com/hy/App.java
+++ src/main/java/com/hy/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("------解决分支测试");
+        System.out.println("------解决分支测试 -- 手动修改");
         System.out.println("----hytest");
         String s = null;
         Map<Character, Integer> map = null;
